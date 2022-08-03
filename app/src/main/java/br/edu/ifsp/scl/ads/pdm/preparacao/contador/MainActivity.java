0b4e29bc51de5232bec03e1303e64e92133ae606
northonh <nobile.pedro@gmail.com>
2022-08-03 16:49:44 -0300

    Ativando viewBinding.

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/preparacao/contador/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/preparacao/contador/MainActivity.java
@@ -1,31 +1,25 @@
 package br.edu.ifsp.scl.ads.pdm.preparacao.contador;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.edu.ifsp.scl.ads.pdm.preparacao.contador.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private Button cliqueBt;
-    private TextView contadorTv;
-    private EditText inicialEt;
+    private ActivityMainBinding activityMainBinding;
     private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
 
-        inicialEt = findViewById(R.id.inicialEt);
-        contadorTv = findViewById(R.id.contadorTv);
-        cliqueBt = findViewById(R.id.cliqueBt);
-
-        inicialEt.addTextChangedListener(new TextWatcher() {
+        activityMainBinding.inicialEt.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -42,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        cliqueBt.setOnClickListener(new View.OnClickListener() {
+        activityMainBinding.cliqueBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                contadorTv.setText(String.valueOf(++contador));
+                activityMainBinding.contadorTv.setText(String.valueOf(++contador));
             }
         });
 
