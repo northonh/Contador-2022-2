bef289b9702fca6bff0f6007b4c14621ef79b5a8
northonh <nobile.pedro@gmail.com>
2022-08-03 16:46:55 -0300

    Adicionando um valor adicional com EditText.

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/preparacao/contador/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/preparacao/contador/MainActivity.java
@@ -3,13 +3,17 @@ package br.edu.ifsp.scl.ads.pdm.preparacao.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button cliqueBt;
     private TextView contadorTv;
+    private EditText inicialEt;
     private int contador = 0;
 
     @Override
@@ -17,8 +21,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inicialEt = findViewById(R.id.inicialEt);
         contadorTv = findViewById(R.id.contadorTv);
         cliqueBt = findViewById(R.id.cliqueBt);
+
+        inicialEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                contador = Integer.parseInt(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         cliqueBt.setOnClickListener(new View.OnClickListener() {
             @Override
