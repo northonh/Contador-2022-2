aa242b1ae9a6d9bd74c6f1c0590510bb12f56d5c
northonh <nobile.pedro@gmail.com>
2022-08-03 16:38:22 -0300

    Contador funcional com Button e TextView

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/preparacao/contador/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/preparacao/contador/MainActivity.java
@@ -3,12 +3,31 @@ package br.edu.ifsp.scl.ads.pdm.preparacao.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button cliqueBt;
+    private TextView contadorTv;
+    private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contadorTv = findViewById(R.id.contadorTv);
+        cliqueBt = findViewById(R.id.cliqueBt);
+
+        cliqueBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contadorTv.setText(String.valueOf(++contador));
+            }
+        });
+
+        // Lambda
+        //cliqueBt.setOnClickListener( (view) -> contadorTv.setText(String.valueOf(++contador)) );
     }
 }
